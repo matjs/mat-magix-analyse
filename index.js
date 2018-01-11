278599d9575009b8f7b942c1fd5329bf7e6aa7af
崇志 <chongzhi.ldq@taobao.com>
2018-01-11 11:13:37 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+/**
+ * 分析js,打包view
+ */
+function analyse(combineTool) {
+    return function* combine(next) {
+        yield next
+        let body = this.body.toString()
+        if (body == 'Not Found') {
+            throw new Error('路径：' + this.path + ' 对应的文件没有找到')
+        }
+        body = yield combineTool.processContent(path.join(__dirname, this.path), '', body)
+        this.body = body.content;
+    }
+}
+
+module.exports = analyse
