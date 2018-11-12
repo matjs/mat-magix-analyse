dcf08beb5a4ccffaedeb90a9b5470400f4f21f16
chongzhi.ldq <chongzhi.ldq@alibaba-inc.com>
2018-11-12 20:34:08 +0800

    Feat: 增加支持将magix-combine的错误发送到浏览器端

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const path = require('path')
+const WebSocket = require('ws')
 
 /**
  * 分析js,打包view
  */
-function analyse(combineTool) {
+function analyse(combineTool, ws) {
     return function* combine(next) {
         yield next
         let body = this.body.toString()
@@ -11,7 +12,21 @@ function analyse(combineTool) {
             throw new Error('路径：' + this.path + ' 对应的文件没有找到')
         }
 
-        body = yield combineTool.processContent(path.join(process.cwd(), this.path), '', body)
+        try {
+            body = yield combineTool.processContent(path.join(process.cwd(), this.path), '', body)
+        } catch (err) {
+            //多窗口多客户端同时发送信息
+            ws.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({
+                        message: err.message,
+                        type: 'error'
+                    }));
+                }
+            })
+        }
+
+
         this.body = body.content;
     }
 }
